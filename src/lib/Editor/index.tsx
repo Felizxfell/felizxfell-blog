3b5fcc5dc759ed7eba22d923fc9d1601e7d60955
Felizxfell <felizxhgf@gmial.com>
2023-08-28 03:27:14 -0600

    add quill js

--- src/lib/Editor/index.tsx
+++ src/lib/Editor/index.tsx
@@ -10,32 +10,30 @@ type PropsQuill = {
   readonly?: boolean
 }
 
+const toolbarOptions = {
+  toolbar: [
+    ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
+    ['blockquote', 'code-block'],
+
+    [{ 'header': 1 }, { 'header': 2 }],               // custom button values
+    ['link', 'blockquote'],                           //'code-block'
+    [{ 'list': 'ordered' }, { 'list': 'bullet' }],
+    //[{ 'script': 'sub'}, { 'script': 'super' }],    // superscript/subscript
+    [{ 'indent': '-1' }, { 'indent': '+1' }],         // outdent/indent
+
+    [{ 'size': ['small', false, 'large', 'huge'] }],  // custom dropdown
+
+    [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+
+    [{ 'color': [] }, { 'background': [] }],          // dropdown with defaults from theme
+    [{ 'align': [] }],
+    //[{ 'direction': 'rtl' }],                         // text direction
+    ['clean']                                         // remove formatting button
+  ]
+}
+
 export default function Quill({ theme = ThemeQuill.snow, readonly = false }: PropsQuill) {
   const [value, setValue] = useState("");
-
-  const toolbarOptions = {
-    toolbar: [
-      ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
-      ['blockquote', 'code-block'],
-
-      [{ 'header': 1 }, { 'header': 2 }],               // custom button values
-      ['link', 'blockquote'],                           //'code-block'
-      [{ 'list': 'ordered' }, { 'list': 'bullet' }],
-      //[{ 'script': 'sub'}, { 'script': 'super' }],    // superscript/subscript
-      [{ 'indent': '-1' }, { 'indent': '+1' }],         // outdent/indent
-
-      [{ 'size': ['small', false, 'large', 'huge'] }],  // custom dropdown
-
-      [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
-
-      [{ 'color': [] }, { 'background': [] }],          // dropdown with defaults from theme
-      [{
-        'align': []
-      }],
-      //[{ 'direction': 'rtl' }],                         // text direction
-      ['clean']                                         // remove formatting button
-    ]
-  }
-
+  
   return <ReactQuill theme={theme} modules={toolbarOptions} value={value} onChange={setValue} readOnly={readonly} />;
 }
