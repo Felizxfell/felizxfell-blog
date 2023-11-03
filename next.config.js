c01d0e7d254de5c351c29893a9c7461d51fd822f
Felizxfell <felizxhgf@gmail.com>
2023-11-02 18:15:31 -0600

    Update version package.json

--- next.config.js
+++ next.config.js
@@ -1,11 +1,8 @@
+const withMDX = require('@next/mdx')();
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    experimental: {
-        mdxRs: true,
-    },
+    pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
 }
 
-const withMDX = require('@next/mdx')(
-
-);
 module.exports = withMDX(nextConfig) // nextConfig
